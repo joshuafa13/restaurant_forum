d8cbf06dbeb42e23be96874a63c5c5fe62a3bac7
Josh Fang <joshuafang13@gmail.com>
2020-09-08 16:11:16 +0800

    feat: add userController and sinup page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminControllers')
+const userController = require('../controllers/userController')
+
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', restController.getRestaurants)
 
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
+
+  app.get('/signUp', userController.signUpPage)
+  app.post('/signUp', userController.signUp)
 }
