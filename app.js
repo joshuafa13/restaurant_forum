cbcb9dc8842b1fab3dfa50ba559bc702445a2206
Josh Fang <joshuafang13@gmail.com>
2020-09-14 18:17:21 +0800

    Add ifCond helper & Update category select in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('handlebars', handlebars())
+app.engine('handlebars', handlebars({ helpers: require('./config/handlebars-helpers') }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
