98174a1d0ce50980f8e9fe0ca36b7e786cc1e609
Josh Fang <joshuafang13@gmail.com>
2020-09-23 15:37:20 +0800

    PUT /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -12,6 +12,7 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 
 router.get('/admin/categories', categoryController.getCategories)
 router.get('/admin/categories/:id', categoryController.getCategories)
